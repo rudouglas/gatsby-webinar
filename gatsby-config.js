705f877ef4be2b9557502fb600e778ab04be3f79
rdouglas <ruairidouglas@gmail.com>
2021-10-27 15:32:03 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,34 +13,8 @@ module.exports = {
     `gatsby-transformer-remark`,
     'gatsby-plugin-image',
     `gatsby-plugin-styled-components`,
-    // {
-		// 	resolve: `gatsby-plugin-manifest`,
-		// 	options: {
-		// 		name: `morty-and-rick-gatsby`,
-		// 		short_name: `morty-rick`,
-		// 		start_url: `/`,
-		// 		background_color: `#663399`,
-		// 		theme_color: `#663399`,
-		// 		display: `minimal-ui`,
-		// 		icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-		// 	},
-		// },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    {
-      resolve: "gatsby-build-newrelic",
-      options: {
-        NR_LICENSE_KEY: process.env.NEW_RELIC_LICENSE_KEY || '',
-        NR_ACCOUNT_ID: process.env.NEW_RELIC_ACCOUNT_ID,
-        SITE_NAME: 'jankstack',
-        customTags: {
-          jankySite: process.env.JANKY_SITE,
-        }
-      }
-    },
     {
       resolve: "@chakra-ui/gatsby-plugin",
       options: {
@@ -56,13 +30,5 @@ module.exports = {
         isUsingColorMode: true,
       },
     },
-    // {
-    //   resolve: `gatsby-source-contentful`,
-    //   options: {
-    //     spaceId: `lyo8oxh9wfuy`,
-    //     // Learn about environment variables: https://gatsby.dev/env-vars
-    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    //   },
-    // },
   ],
 }
